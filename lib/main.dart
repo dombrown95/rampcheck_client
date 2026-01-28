fc3e394937bdbfaa7b86c0957cf2f22583a7c811
Dom Brown <dominicbrown4@hotmail.com>
2026-01-28 10:19:43 +0000

    fix sql import to resolve error

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'dart:io' show Platform;
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:sqflite/sqflite.dart';
 import 'data/local/local_store.dart';
 import 'models/job.dart';
 
